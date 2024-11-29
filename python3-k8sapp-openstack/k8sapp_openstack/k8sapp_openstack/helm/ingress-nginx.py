432cb0b7d50fafaa9eeb6b3578abcae23d15c16d
Daniel Caires <DanielMarques.Caires@windriver.com>
2024-11-29 09:49:44 -0300

    Create Ingress-nginx FluxCD Manifest
    The Openstack upstream community have deprecated its
    Ingress Helm chart and begun to use the Helm chart from
    Nginx.
    
    The purpose of this task is to add a new FluxCD Manifest
    for the Ingress-nginx Helm chart without removing the
    current Ingress manifest. There will be a follow-up to
    this review that will substitute the current ingress
    manifest with the new one. Because of that, ingress-nginx
    is not being added to the kustomization file, as it is
    not to be deployed right now.
    
    The plugin for this new Helm chart is also added to the
    helm folder.
    
    Test Plan:
    PASS - Build stx-openstack-helm-fluxcd and STX-O
    PASS - Ingress-nginx Helm chart appears in the build
    PASS - Update and apply STX-O
    PASS - Ingress-nginx is present but not deployed
    
    Story: 2011303
    Task: 51430
    
    Change-Id: Iaf3cb33724871141abb5f8334b5043d3b823041b
    Signed-off-by: Daniel Caires <DanielMarques.Caires@windriver.com>

---
+++ python3-k8sapp-openstack/k8sapp_openstack/k8sapp_openstack/helm/ingress-nginx.py
@@ -0,0 +1,54 @@
+#
+# Copyright (c) 2024 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from sysinv.common import exception
+from sysinv.helm import common
+
+from k8sapp_openstack.common import constants as app_constants
+from k8sapp_openstack.helm import openstack
+
+
+class IngressHelm(openstack.OpenstackBaseHelm):
+    """Class to encapsulate helm operations for the ingress chart"""
+
+    CHART = app_constants.HELM_CHART_INGRESS
+    HELM_RELEASE = app_constants.FLUXCD_HELMRELEASE_INGRESS
+
+    def get_overrides(self, namespace=None):
+        limit_enabled, limit_cpus, limit_mem_mib = self._get_platform_res_limit()
+
+        overrides = {
+            common.HELM_NS_OPENSTACK: {
+                'controller': {
+                    'replicaCount': self._num_provisioned_controllers(),
+                    'resources': {
+                        'enabled': limit_enabled,
+                        'limits': {
+                            'cpu': "%d000m" % (limit_cpus),
+                            'memory': "%dMi" % (limit_mem_mib)
+                        }
+                    }
+                },
+                'defaultBackend': {
+                    'replicaCount': self._num_provisioned_controllers(),
+                    'resources': {
+                        'enabled': limit_enabled,
+                        'limits': {
+                            'cpu': "%d000m" % (limit_cpus),
+                            'memory': "%dMi" % (limit_mem_mib)
+                        }
+                    }
+                }
+            }
+        }
+
+        if namespace in self.SUPPORTED_NAMESPACES:
+            return overrides[namespace]
+        elif namespace:
+            raise exception.InvalidHelmNamespace(chart=self.CHART,
+                                                 namespace=namespace)
+        else:
+            return overrides

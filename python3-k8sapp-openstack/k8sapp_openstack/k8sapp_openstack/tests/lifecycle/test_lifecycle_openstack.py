223260bd51da42e7f6570b01de87cb2e4ac40e39
jbarboza <joaopedro.barbozalioneza@windriver.com>
2025-06-02 17:18:45 -0300

    Added new unit tests for lifecycle methods
    This review added new unit tests for lifecycle plugin
    methods that were not being covered yet.
    
    TEST PLAN:
    - PASS: Unit tests are running and passing
    - PASS: Code coverage is above 80%
    
    Story: 2011473
    Task: 52287
    
    Change-Id: I8a571b0419bf5954a58ed6d2ef52e5eae35d66a0
    Signed-off-by: jbarboza <joaopedro.barbozalioneza@windriver.com>

--- python3-k8sapp-openstack/k8sapp_openstack/k8sapp_openstack/tests/lifecycle/test_lifecycle_openstack.py
+++ python3-k8sapp-openstack/k8sapp_openstack/k8sapp_openstack/tests/lifecycle/test_lifecycle_openstack.py
@@ -881,3 +881,206 @@ class OpenstackAppLifecycleOperatorTest(dbbase.BaseHostTestCase):
             "there are conflicting vswitch configurations: "
             "openvswitch-dpdk=enabled, openvswitch=enabled, other-vswitch=enabled$"
         )
+
+    @mock.patch('k8sapp_openstack.helpers.ldap.check_group', return_value=False)
+    @mock.patch('k8sapp_openstack.helpers.ldap.add_group', return_value=True)
+    @mock.patch('k8sapp_openstack.utils.create_clients_working_directory', return_value=True)
+    @mock.patch('k8sapp_openstack.utils.get_clients_working_directory', return_value='/custom/path')
+    @mock.patch('k8sapp_openstack.lifecycle.lifecycle_openstack.lifecycle_utils.create_local_registry_secrets')
+    @mock.patch('sysinv.common.kubernetes.KubeOperator')
+    def test_create_app_specific_resources_pre_apply_success(
+            self,
+            mock_kube_operator,
+            mock_create_local_registry_secrets,
+            mock_get_working_dir,
+            mock_create_dir,
+            mock_add_group,
+            mock_check_group):
+        """ Test the pre-apply actions for creating app-specific resources. """
+
+        app_op = mock.Mock()
+        app = mock.Mock(name='test_app', version='1.0')
+        hook_info = mock.Mock()
+
+        mock_kube = mock_kube_operator.return_value
+        mock_kube.kube_get_config_map.return_value = True
+        mock_kube.kube_read_config_map.return_value = mock.Mock()
+
+        self.lifecycle._create_app_specific_resources_pre_apply(app_op, app, hook_info)
+
+        mock_create_local_registry_secrets.assert_called_once()
+        mock_kube.kube_delete_config_map.assert_called_once()
+        mock_kube.kube_create_config_map.assert_called_once()
+        mock_check_group.assert_called_once()
+        mock_add_group.assert_called_once()
+        mock_create_dir.assert_called_once_with(path='/custom/path')
+
+    @mock.patch('k8sapp_openstack.helpers.ldap.check_group', return_value=False)
+    @mock.patch('k8sapp_openstack.helpers.ldap.add_group', return_value=True)
+    @mock.patch('k8sapp_openstack.utils.create_clients_working_directory', return_value=True)
+    @mock.patch('k8sapp_openstack.utils.get_clients_working_directory', return_value='/custom/path')
+    @mock.patch('k8sapp_openstack.lifecycle.lifecycle_openstack.lifecycle_utils.create_local_registry_secrets')
+    @mock.patch('sysinv.common.kubernetes.KubeOperator')
+    def test_create_app_specific_resources_pre_apply_failed(
+            self,
+            mock_kube_operator,
+            mock_create_local_registry_secrets,
+            mock_get_working_dir,
+            mock_create_dir,
+            mock_add_group,
+            mock_check_group):
+        """ Test the pre-apply actions for creating app-specific resources with a failure. """
+
+        mock_kube = mock_kube_operator.return_value
+        mock_kube.kube_get_config_map.return_value = True
+        mock_kube.kube_read_config_map.return_value = False
+
+        app_op = mock.Mock()
+        app = mock.Mock(name='test_app', version='1.0')
+        hook_info = mock.Mock()
+
+        self.assertRaises(
+            exception.LifecycleMissingInfo,
+            self.lifecycle._create_app_specific_resources_pre_apply,
+            app_op,
+            app,
+            hook_info
+        )
+
+    @mock.patch('k8sapp_openstack.lifecycle.lifecycle_openstack.lifecycle_utils')
+    @mock.patch(
+        'k8sapp_openstack.lifecycle.lifecycle_openstack.OpenstackAppLifecycleOperator._post_remove_ldap_actions'
+    )
+    def test_delete_app_specific_resources_post_remove(
+            self,
+            mock_post_remove_ldap_actions,
+            mock_lifecycle_utils):
+        """ Test the post-remove actions for deleting app-specific resources. """
+
+        app_op = mock.Mock()
+        app = mock.Mock()
+        hook_info = mock.Mock()
+
+        self.lifecycle._delete_app_specific_resources_post_remove(app_op, app, hook_info)
+
+        mock_lifecycle_utils.delete_local_registry_secrets.assert_called_once()
+        mock_lifecycle_utils.delete_persistent_volume_claim.assert_called_once()
+        mock_lifecycle_utils.delete_configmap.assert_called_once()
+        mock_lifecycle_utils.delete_namespace.assert_called_once()
+        mock_post_remove_ldap_actions.assert_called_once()
+
+    @mock.patch('k8sapp_openstack.lifecycle.lifecycle_openstack.utils.HostHelper.get_active_controller')
+    @mock.patch(
+        'k8sapp_openstack.lifecycle.lifecycle_openstack.utils.is_host_simplex_controller',
+        return_value=True
+    )
+    def test_pre_apply_check_fail_vim_services(self, mock_simplex, mock_get_active_controller):
+        """ Test the pre-apply check for VIM services when they are not enabled. """
+
+        active_controller = mock.Mock(vim_progress_status="not-enabled")
+        active_controller.hostname = 'controller-0'
+        mock_get_active_controller.return_value = active_controller
+
+        self.assertRaises(
+            exception.LifecycleSemanticCheckException,
+            self.lifecycle._pre_apply_check,
+            mock.Mock(),
+            mock.Mock(name='test_app'),
+            mock.Mock()
+        )
+
+    @mock.patch('k8sapp_openstack.helpers.ldap.check_group', return_value=False)
+    @mock.patch('k8sapp_openstack.helpers.ldap.add_group', return_value=True)
+    @mock.patch('k8sapp_openstack.utils.create_clients_working_directory', return_value=True)
+    @mock.patch('k8sapp_openstack.utils.get_clients_working_directory', return_value='/custom/path')
+    def test_pre_apply_ldap_actions_success(
+            self,
+            mock_get_dir,
+            mock_create_dir,
+            mock_add_group,
+            mock_check_group):
+        """ Test the pre-apply LDAP actions for creating a group and directory. """
+
+        app = mock.Mock(name='test_app', version='1.0')
+
+        self.lifecycle._pre_apply_ldap_actions(app)
+
+        mock_check_group.assert_called_once()
+        mock_add_group.assert_called_once()
+        mock_create_dir.assert_called_once_with(path='/custom/path')
+
+    @mock.patch('k8sapp_openstack.helpers.ldap.check_group', return_value=True)
+    @mock.patch('k8sapp_openstack.helpers.ldap.delete_group')
+    @mock.patch('k8sapp_openstack.utils.delete_clients_working_directory', return_value=True)
+    def test_post_remove_ldap_actions_success(self, mock_delete_dir, mock_delete_group, mock_check_group):
+        """ Test the post-remove LDAP actions for deleting a group and directory. """
+
+        self.lifecycle._post_remove_ldap_actions()
+
+        mock_delete_dir.assert_called_once()
+        mock_check_group.assert_called_once()
+        mock_delete_group.assert_called_once()
+
+    def test__post_remove_missing_extra(self):
+        """ Test post_remove with missing extra information. """
+
+        context = mock.Mock()
+        conductor_obj = mock.Mock()
+        hook_info = {}
+
+        self.assertRaises(
+            exception.LifecycleMissingInfo,
+            self.lifecycle.post_remove,
+            context,
+            conductor_obj,
+            hook_info
+        )
+
+    def test__post_remove_missing_app_removed(self):
+        """ Test post_remove with missing app_removed information. """
+
+        context = mock.Mock()
+        conductor_obj = mock.Mock()
+        hook_info = {
+            LifecycleConstants.EXTRA: {}
+        }
+
+        self.assertRaises(
+            exception.LifecycleMissingInfo,
+            self.lifecycle.post_remove,
+            context,
+            conductor_obj,
+            hook_info
+        )
+
+    def test__post_remove_app_removed_true(self):
+        """ Test post_remove with app_removed set to True. """
+
+        context = mock.Mock()
+        conductor_obj = mock.Mock()
+        hook_info = {
+            LifecycleConstants.EXTRA: {
+                LifecycleConstants.APP_REMOVED: True
+            }
+        }
+
+        self.lifecycle.post_remove(context, conductor_obj, hook_info)
+
+        conductor_obj._update_vim_config.assert_called_once_with(context)
+        conductor_obj._update_radosgw_config.assert_called_once_with(context)
+
+    def test__post_remove_app_removed_false(self):
+        """ Test post_remove with app_removed set to False. """
+
+        context = mock.Mock()
+        conductor_obj = mock.Mock()
+        hook_info = {
+            LifecycleConstants.EXTRA: {
+                LifecycleConstants.APP_REMOVED: False
+            }
+        }
+
+        self.lifecycle.post_remove(context, conductor_obj, hook_info)
+
+        conductor_obj._update_vim_config.assert_not_called()
+        conductor_obj._update_radosgw_config.assert_not_called()

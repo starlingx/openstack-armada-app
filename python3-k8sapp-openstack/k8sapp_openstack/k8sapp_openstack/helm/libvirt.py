e8f624f579698890c515679d82eeb29c46a11341
Daniel Caires <DanielMarques.Caires@windriver.com>
2026-04-22 13:54:11 -0300

    Remove per-host override from libvirt plugin
    The libvirt helm plugin was generating a per-compute-node override
    containing the pod IP to set listen_address in libvirt.conf. Since
    every node has a unique IP, the aggregation logic could never group
    nodes together, producing one aggregate per node. This caused a
    comparison overhead during override evaluation, resulting in
    significant delays on systems with 100+ compute nodes.
    
    This review removes the per-host override logic (_get_per_host_overrides
    and _update_host_addresses) from the libvirt helm plugin.
    Instead, expose the host IP via the CLUSTER_HOST_IP environment
    variable in the libvirt daemonset (using status.hostIP), following
    the same approach used for nova in patch 0026. The dynamic options
    script, in libvirt, now falls back to CLUSTER_HOST_IP when neither
    listen_interface nor listen_address is explicitly configured.
    
    This brings libvirt in line with how nova and neutron handle
    per-node IP configuration and eliminates the scaling bottleneck
    during reapply operations.
    
    Test Plan:
    PASS - Build all packages and STX-O tarball
    PASS - Upload and apply STX-Openstack
    PASS - Overrides for libvirt generated correctly
    PASS - At 46 compute nodes libvirt generation overrides time reduced
           from ~60s to ~1 second
    PASS - Reapply evaluation after node reboot at around ~3 seconds.
    PASS - Reapply evaluation during scale operation between 2 and
           3 seconds
    
    Closes-Bug: #2151229
    
    Change-Id: I765e60d1240533a93b1eb93f74e250fbc5b52742
    Signed-off-by: Daniel Caires <DanielMarques.Caires@windriver.com>

--- python3-k8sapp-openstack/k8sapp_openstack/k8sapp_openstack/helm/libvirt.py
+++ python3-k8sapp-openstack/k8sapp_openstack/k8sapp_openstack/helm/libvirt.py
@@ -7,7 +7,6 @@
 from oslo_log import log as logging
 from sysinv.common import constants
 from sysinv.common import exception
-from sysinv.common import utils
 from sysinv.helm import common
 
 from k8sapp_openstack.common import constants as app_constants
@@ -29,15 +28,8 @@ class LibvirtHelm(openstack.OpenstackBaseHelm):
 
     def __init__(self, operator):
         super(LibvirtHelm, self).__init__(operator)
-        self.fallback_conf = {
-            'listen_addr': '0.0.0.0',
-        }
-        self.labels_by_hostid = {}
-        self.addresses_by_hostid = {}
 
     def get_overrides(self, namespace=None):
-        self.labels_by_hostid = self._get_host_labels()
-        self.addresses_by_hostid = self._get_host_addresses()
         nova_shares = self._get_instances_nfs_shares_config()
 
         pvc_resolution = self._resolve_nova_pvc_overrides()
@@ -151,11 +143,6 @@ class LibvirtHelm(openstack.OpenstackBaseHelm):
                 'admin_keyring': admin_keyring,
                 'cinder': cinder_overrides,
             },
-            'overrides': {
-                'libvirt_libvirt': {
-                    'hosts': self._get_per_host_overrides()
-                }
-            },
         }
 
         ceph_uuid = get_ceph_fsid()
@@ -165,37 +152,3 @@ class LibvirtHelm(openstack.OpenstackBaseHelm):
             }
 
         return overrides
-
-    def _update_host_addresses(self, host, libvirt_config):
-        cluster_host_ip = self._get_cluster_host_ip(
-            host, self.addresses_by_hostid)
-        if cluster_host_ip is None:
-            return
-
-        libvirt_config.update({'listen_addr': cluster_host_ip})
-
-    def _get_per_host_overrides(self):
-        host_list = []
-        hosts = self.dbapi.ihost_get_list()
-
-        for host in hosts:
-            host_labels = self.labels_by_hostid.get(host.id, [])
-            if (host.invprovision in [constants.PROVISIONED,
-                                      constants.PROVISIONING] or
-                    host.ihost_action in [constants.UNLOCK_ACTION,
-                                          constants.FORCE_UNLOCK_ACTION]):
-                if (constants.WORKER in utils.get_personalities(host) and
-                        utils.has_openstack_compute(host_labels)):
-
-                    hostname = str(host.hostname)
-                    libvirt_conf = {}
-                    self._update_host_addresses(host, libvirt_conf)
-                    host_nova = {
-                        'name': hostname,
-                        'conf': {
-                            'libvirt': libvirt_conf if libvirt_conf else self.fallback_conf,
-                        }
-                    }
-                    host_list.append(host_nova)
-
-        return host_list
